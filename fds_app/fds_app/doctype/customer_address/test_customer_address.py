8823ea8520204474d606fa84fc4e96c1e91cdd6a
Abdulrhman-Khaled <bodyono3@gmail.com>
2026-02-16 13:38:43 +0000

    migrate all doctypes from next

---
+++ fds_app/fds_app/doctype/customer_address/test_customer_address.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, BodyKh and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomerAddress(FrappeTestCase):
+	pass
